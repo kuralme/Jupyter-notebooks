0edf3483f50b6dde37453e2c65209164c7d40557
Ege <kuralme@protonmail.com>
2021-08-02 02:03:01 +0300

    Update W1.py

--- DeepLearning/W1.py
+++ DeepLearning/W1.py
@@ -1,1 +1,32 @@
 import numpy
+
+a = np.array([1,2,3,4])
+print(a)
+
+#----------------------------------------------------------------------#
+# Execution time difference between simple loop and vector operations
+#----------------------------------------------------------------------#
+import time
+
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+tic = time.time()
+c = np.dot(a,b)
+toc = time.time()
+        
+print("Vectorized ver: "+str(1000*(toc-tic))+"ms")
+
+c = 0
+
+tic = time.time()
+for i in range(1000000):
+    c += a[i]*b[i]
+toc = time.time()
+
+print("For loop: "+ str(1000*(toc-tic))+"ms")
+
+
+#----------------------------------------------------------------------#
+#  
+#----------------------------------------------------------------------#
